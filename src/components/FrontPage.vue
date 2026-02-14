e13cd9516585dfdd01c490fd1f2e19b0dcde2dd2
pyag <yagnikpriyash@gmail.com>
2026-02-14 10:17:31 +0530

    Added lucide icons and modified

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import { Search } from "lucide-vue-next";
 
 import Button from "./Button.vue";
 import Modal from "./Modal.vue";
@@ -26,9 +27,7 @@ const showLoginModal = ref(false);
             <Input :placeholder="`Search lockers`" :extra-div-classes="`!rounded-full w-3xl max-w-3xl focus-within:ring-gray-400`"
                 :extra-input-classes="`!py-2 !px-4`">
                 <Button extra-classes="!border-none !text-black !bg-gray-200 hover:!bg-gray-300 !rounded-full !bg-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
+                    <Search class="h-6 w-6 text-gray-500" />
                 </Button>
             </Input>
         </div>
