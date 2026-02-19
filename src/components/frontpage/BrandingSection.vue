70809508a59496f4b8417d6fd1ba6322c70c80a0
pyag <yagnikpriyash@gmail.com>
2026-02-19 11:36:26 +0530

    Changed the page title to logie and some minor change

--- src/components/frontpage/BrandingSection.vue
+++ src/components/frontpage/BrandingSection.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { Facebook, TwitterIcon, GithubIcon, InstagramIcon } from "lucide-vue-next";
 
 import BaseButton from "../BaseButton.vue";
