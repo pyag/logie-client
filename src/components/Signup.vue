9867b8f8b6531700c26c59dc22322f5e60af7afa
pyag <yagnikpriyash@gmail.com>
2026-04-30 18:43:12 +0530

    Fixed some minor correction

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -101,7 +101,7 @@ const createLocker = async () => {
         <Password :required=true id="cnfrm-password" v-model="cnfrm_password" name="cnfrm-password" placeholder="Confirm password" />
 
         <Input type="text" id="email" v-model="email" name="email"
-        placeholder="Email or Phone (optional)" />
+        placeholder="Email (optional)" />
 
         <div v-if="errorMessage" class="text-red-700 text-base/5">{{ errorMessage }}</div>
 
