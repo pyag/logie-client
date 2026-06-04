c6ededfc6436d91d21674ed383066e47b2875071
pyag <yagnikpriyash@gmail.com>
2026-06-04 16:43:28 +0530

    Added search functionality to redirect in user dashboard also

--- src/components/dashboard/NavBar.vue
+++ src/components/dashboard/NavBar.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { Search } from 'lucide-vue-next';
 import { useUserStore } from '../../stores/user';
 import Input from '../Input.vue';
+import Button from '../Button.vue';
 import UserOptions from './UserOptions.vue'
 
 const userStore = useUserStore();
+const router = useRouter();
+const searchQuery = ref('');
+
+const submitSearch = () => {
+    const query = searchQuery.value.trim();
+    if (!query) {
+        return;
+    }
+    router.push({ name: 'search', query: { name: query } });
+};
 </script>
 
 <template>
@@ -14,9 +28,18 @@ const userStore = useUserStore();
             </h1>
         </div>
         <div class="p-3 w-1/3 content-center">
-            <Input extra-div-classes="rounded-full! focus-within:ring-blue-200! border-gray-500! border!"
+            <Input
+                v-model:modelValue="searchQuery"
+                @keydown.enter="submitSearch"
+                extra-div-classes="rounded-full! focus-within:ring-blue-200! border-gray-500! border!"
                 extra-input-classes="pl-5"
-                placeholder="Search other locker..." />
+                placeholder="Search other locker...">
+                <Button
+                    @click="submitSearch"
+                    extra-classes="!border-none !text-black !bg-gray-200 hover:!bg-gray-300 !rounded-full !bg-none">
+                    <Search class="h-5 w-5" />
+                </Button>
+            </Input>
         </div>
         <div class="ml-auto mr-3 p-3">
             <UserOptions @logout="$emit('logout')" />
